f20036e164b850a1063bd5af79d1ad01c05225d4
Andrew Somerville <andy.somerville@gmail.com>
2014-04-19 22:08:18 +0000

    add virtualenv stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+virtualenv ./env
+. ./env/bin/activate
+pip install --pre -r requirements.txt
+
 script_path="$(readlink -f ./pull_and_parse.py)"
 echo "0 5 * * 1 $script_path 2>> pull_and_parse.log" > cronentry
 
